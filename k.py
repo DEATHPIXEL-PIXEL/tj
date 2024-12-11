ca0af97c77cb2c1a26e93e33f3b8ebbeb7df50bb
DEATHPIXEL-PIXEL <zayaankhan337@gmail.com>
2024-12-11 18:51:32 +0530

    first commit

---
+++ k.py
@@ -0,0 +1,11 @@
+age = input("Please enter your age: ")
+
+if :
+
+    age = int(age)
+    if age % 2 == 0:
+        print("Your age" ,age ,"is even")
+    else:
+        print("Your age", age ,   "is odd.")
+else:
+    print("Error: Please enter a valid positive integer for age.")
